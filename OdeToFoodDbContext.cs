ffecd0f3a18c2802292d60931f9badec437b4ba8
Yassine Naji <shinobinaji@gmail.com>
2022-03-28 14:29:57 +0000

    Add entityFramework

---
+++ OdeToFoodDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options) : base(options)
+        {
+
+        }
+
+       public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
---
+++ OdeToFoodDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContextFactory : IDesignTimeDbContextFactory<OdeToFoodDbContext>
+    {
+        public OdeToFoodDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OdeToFoodDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=OdeToFood;Integrated Security=True");
+
+            return new OdeToFoodDbContext(optionsBuilder.Options);
+        }
+    }
+}
